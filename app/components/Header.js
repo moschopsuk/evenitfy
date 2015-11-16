cfc1da937ab09183e2e8ebe6f1760f7443555e42
Luke Moscrop <luke.moscrop@bbc.co.uk>
2015-11-16 19:15:26 +0000

    event header updated

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,12 +9,6 @@ class Header extends React.Component {
                         <h1>Live Stream test page!</h1>
                         <h3>Summary of the events!</h3>
                         <i className="fa fa-clock-o"></i> 2nd September 2015 16:00
-
-                        <ul className="list-inline tags">
-                            <li><a href="#" className="name">test<span className="number">5</span></a></li>
-                            <li><a href="#" className="name">test2<span className="number">43</span></a></li>
-                            <li><a href="#" className="name">test3<span className="number">53</span></a></li>
-                        </ul>
                     </div>
                 </div>
             </header>
